3c17f04b0c94a5edafb3d81f369cee8b556baad9
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-06-26 19:32:03 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,3 @@
+# # You have a employee.txt text file containing employee data. Open the file provided to see the data.
+# write a program to give the bonus == 2000 for the employees whose salary is more than 35000.
+# Modify it into result.txt
