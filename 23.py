43b359f05665cb3b6c8029c77e0b23a84ce37dca
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-07-08 10:24:37 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,7 +1,10 @@
 # Find 2nd larget element in list
 
-# Way-1
+# Way-1: Sort and print 2nd largest number
 lis = [23,12,45,43,67,64]
 lis.sort()
-print(lis)
-print(lis[-2])
+print(f"Second largest number: {lis[-2]}")
+
+# Way-2: Remove largest number and print max
+lis.pop()
+print(f"Second largest number: {lis[-1]}")
